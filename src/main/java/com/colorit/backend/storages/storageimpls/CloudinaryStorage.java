2583661dae672f096bf05634a55be29c0100ce9c
HustonMavr <huston.mavr@gmail.com>
2018-04-03 14:12:19 +0300

    added storage

--- src/main/java/com/colorit/backend/storages/storageimpls/CloudinaryStorage.java
+++ src/main/java/com/colorit/backend/storages/storageimpls/CloudinaryStorage.java
@@ -3,12 +3,16 @@ package com.colorit.backend.storages.storageimpls;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.colorit.backend.storages.IStorage;
+import org.cloudinary.json.JSONException;
 import org.cloudinary.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
 
 @Component
@@ -21,23 +25,25 @@ public class CloudinaryStorage implements IStorage {
 
     public CloudinaryStorage(@Value("${cloud_name}") String cloudName,
                       @Value("${api_key}") String apiKey,
-                      @Value("${api_secret}") String apiSeret) {
+                      @Value("${api_secret}") String apiSecret) {
         this.cloudName = cloudName;
-        this.apiSecret = apiSeret;
+        this.apiSecret = apiSecret;
         this.apiKey = apiKey;
         this.cloudinary = new Cloudinary("cloudinary://" + apiKey + ':' + apiSecret + '@' + cloudName);
 
     }
 
     @Override
-    public String writeFile(File file) throws Exception {
+    public String writeFile(File file) throws IOException, JSONException {
         final Map cloudResponse = this.cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
         final JSONObject json = new JSONObject(cloudResponse);
         return json.getString("url");
     }
 
     @Override
-    public byte[] readFile(String file) {
-        return null;
+    public byte[] readFile(String path) throws IOException {
+        try(InputStream in = new URL(path).openStream()){
+            return in.readAllBytes();
+        }
     }
 }
